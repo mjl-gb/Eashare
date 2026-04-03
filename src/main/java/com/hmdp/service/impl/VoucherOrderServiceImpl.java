0f38ef9b95b643a0e7f9841433bf27b778d78020
me <19863756685@163.com>
2026-04-03 21:15:07 +0800

    添加优惠券，实现秒杀下单，解决超卖、一人一单问题

--- src/main/java/com/hmdp/service/impl/VoucherOrderServiceImpl.java
+++ src/main/java/com/hmdp/service/impl/VoucherOrderServiceImpl.java
@@ -1,14 +1,25 @@
 package com.hmdp.service.impl;
 
+import com.hmdp.dto.Result;
+import com.hmdp.entity.SeckillVoucher;
+import com.hmdp.entity.Voucher;
 import com.hmdp.entity.VoucherOrder;
 import com.hmdp.mapper.VoucherOrderMapper;
+import com.hmdp.service.ISeckillVoucherService;
 import com.hmdp.service.IVoucherOrderService;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.hmdp.utils.RedisIdWorker;
+import com.hmdp.utils.UserHolder;
+import org.springframework.aop.framework.AopContext;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import javax.annotation.Resource;
+import java.time.LocalDateTime;
 
 /**
  * <p>
- *  服务实现类
+ * 服务实现类
  * </p>
  *
  * @author 虎哥
@@ -16,5 +27,71 @@ import org.springframework.stereotype.Service;
  */
 @Service
 public class VoucherOrderServiceImpl extends ServiceImpl<VoucherOrderMapper, VoucherOrder> implements IVoucherOrderService {
+    @Resource
+    private ISeckillVoucherService seckillVoucherService;
+    @Resource
+    private RedisIdWorker redisIdWorker;
 
+    @Override
+    public Result seckillVoucher(Long voucherId) {
+        //1.查询优惠券
+        SeckillVoucher voucher = seckillVoucherService.getById(voucherId);
+        //2.判断秒杀是否开始
+        if (voucher.getBeginTime().isAfter(LocalDateTime.now())) {
+            return Result.fail("秒杀尚未开始");
+        }
+        //3.判断秒杀是否结束
+        if (voucher.getEndTime().isBefore(LocalDateTime.now())) {
+            return Result.fail("秒杀已结束");
+        }
+
+        //4.判断库存是否充足
+        if (voucher.getStock() < 1) {
+            return Result.fail("库存不足");
+        }
+        Long userId = UserHolder.getUser().getId();
+
+        synchronized (userId.toString().intern()) {
+            //拿到当前代理对象，保证spring的事务能被代理对象执行（而不是this关键字拿到的当前VoucherOrderServiceImpl对象）
+            //避免没提交事务就释放锁
+            IVoucherOrderService proxy = (IVoucherOrderService) AopContext.currentProxy();
+            return proxy.createVoucherOrder(voucherId);
+        }
+    }
+
+    @Transactional
+    public Result createVoucherOrder(Long voucherId) {
+        //5.一人一单
+        Long userId = UserHolder.getUser().getId();
+        //5.1查询订单
+        int count = query().eq("user_id", userId).eq("voucher_id", voucherId).count();
+        //5.2判断订单是否存在
+        if (count > 0) {
+            return Result.fail("用户已购买过该优惠券");
+        }
+        //6.扣减库存
+        boolean success = seckillVoucherService.update()
+                .setSql("stock = stock - 1")
+                .eq("voucher_id", voucherId)
+                .gt("stock", 0)
+                .update();
+        if (!success) {
+            return Result.fail("库存不足");
+        }
+
+        //7.创建订单
+        VoucherOrder vO = new VoucherOrder();
+        //7.1订单id
+        Long orderId = redisIdWorker.nextId("order");
+        vO.setId(orderId);
+        //7.2用户id
+        vO.setUserId(UserHolder.getUser().getId());
+        //7.3代金券id
+        vO.setVoucherId(voucherId);
+        save(vO);
+        //8.返回订单号id
+        return Result.ok(orderId);
+
+
+    }
 }
--- src/test/java/com/hmdp/HmDianPingApplicationTests.java
+++ src/test/java/com/hmdp/HmDianPingApplicationTests.java
@@ -3,10 +3,14 @@ package com.hmdp;
 import com.hmdp.entity.Shop;
 import com.hmdp.service.impl.ShopServiceImpl;
 import com.hmdp.utils.ChaceClient;
+import com.hmdp.utils.RedisIdWorker;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import javax.annotation.Resource;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 import java.util.concurrent.TimeUnit;
 
 import static com.hmdp.utils.RedisConstants.CACHE_SHOP_KEY;
@@ -17,10 +21,33 @@ class HmDianPingApplicationTests {
     private ShopServiceImpl shopService;
     @Resource
     private ChaceClient chaceClient;
+    @Resource
+    private RedisIdWorker redisIdWorker;
+    private static final ExecutorService CACHE_REBUILD_EXECUTOR = Executors.newFixedThreadPool(500);
     @Test
     void testSaveShop() throws InterruptedException {
         Shop shop = shopService.getById(1);
         chaceClient.setWithLogicalExpire(CACHE_SHOP_KEY + 1, shop, 1L, TimeUnit.SECONDS);
+    }
+
+
+    @Test
+    void testIdWorker() throws InterruptedException {
+        CountDownLatch latch = new CountDownLatch(300);
+       Runnable task = () -> {
+           for (int i = 0; i < 100; i++) {
+               long id = redisIdWorker.nextId("order");
+               System.out.println("id = " + id);
+           }
+           latch.countDown();
+       };
+       long start = System.currentTimeMillis();
+       for(int i = 0; i < 300; i++){
+           CACHE_REBUILD_EXECUTOR.submit(task);
+       }
+       latch.await();
+       long end = System.currentTimeMillis();
+       System.out.println("time = " + (end - start));
     }
 
 
